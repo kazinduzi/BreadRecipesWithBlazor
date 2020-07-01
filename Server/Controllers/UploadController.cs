d05de36e9f7c3139985ee107cb960b802ae66724
Emmanuel Ndayiragije <endayiragije@gmail.com>
2020-07-01 23:34:56 +0200

    Adding BlazorInputFile

---
+++ Server/Controllers/UploadController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Internal;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorAppWASM.Server.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class UploadController: ControllerBase
+	{
+		private readonly IWebHostEnvironment _env;
+
+		public UploadController(IWebHostEnvironment env)
+		{
+			_env = env;
+		}
+
+		public async Task Post()
+		{
+			if (HttpContext.Request.Form.Files.Any())
+			{
+				var files = HttpContext.Request.Form.Files.ToList();
+				foreach(var file in files)
+				{
+					var path = Path.Combine(_env.ContentRootPath, "Upload", file.FileName);
+
+					if (!Directory.Exists(path))
+					{
+						Directory.CreateDirectory(Path.GetDirectoryName(path));
+					}						
+
+					using var stream = new FileStream(path, FileMode.Create);
+					await file.CopyToAsync(stream);
+				}
+			}
+		}
+	}
+}
