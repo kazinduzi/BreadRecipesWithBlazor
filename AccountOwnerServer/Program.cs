a2eed418b222940e0d9055be51f5fa002e9de266
Kazinduzi <endayiragije@gmail.com>
2020-09-18 22:16:24 +0200

    Update Program.cs

--- AccountOwnerServer/Program.cs
+++ AccountOwnerServer/Program.cs
@@ -16,11 +16,15 @@ namespace AccountOwnerServer
 			CreateHostBuilder(args).Build().Run();
 		}
 
-		public static IHostBuilder CreateHostBuilder(string[] args) =>
+		public static IHostBuilder CreateHostBuilder(string[] args) => 
 			Host.CreateDefaultBuilder(args)
 				.ConfigureWebHostDefaults(webBuilder =>
 				{
+					if (env == EnvironmentName.Staging || env == EnvironmentName.Production){
+						builder.UseIIS();
+					}
 					webBuilder.UseStartup<Startup>();
 				});
+			
 	}
 }
